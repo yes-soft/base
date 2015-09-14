5a048a84ffbdb098f07e97ca28b04420b558ea7b
vellengs <vellengs@qq.com>
2015-09-14 14:03:53 +0800

    主题开发

--- src/plugins/base/directives/forms.js
+++ src/plugins/base/directives/forms.js
@@ -20,13 +20,23 @@
         ['schemaFormProvider', 'schemaFormDecoratorsProvider', 'sfPathProvider',
             function (schemaFormProvider, schemaFormDecoratorsProvider, sfPathProvider) {
 
+                var uploader = function(name, schema, options) {
+                    if (schema.type === 'string' && schema.format == 'uploader') {
+                        var f = schemaFormProvider.stdFormObj(name, schema, options);
+                        f.key  = options.path;
+                        f.type = 'uploader';
+                        options.lookup[sfPathProvider.stringify(options.path)] = f;
+                        return f;
+                    }
+                };
+
                 schemaFormDecoratorsProvider.addMapping(
                     'bootstrapDecorator',
                     'uploader',
                     "plugins/base/templates/forms/uploader.html"
                 );
                 schemaFormDecoratorsProvider.createDirective(
-                    'select2',
+                    'uploader',
                     "plugins/base/templates/forms/uploader.html"
                 );
             }
