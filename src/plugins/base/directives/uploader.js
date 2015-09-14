34dabb57bde1bbfa7d345a6983f19c449a7d711b
qiongwei.cai@yes-soft.cn <245104777@qq.com>
2015-09-14 18:29:12 +0800

    附件选择器ok

--- src/plugins/base/directives/uploader.js
+++ src/plugins/base/directives/uploader.js
@@ -6,33 +6,67 @@
 
                 return {
                     restrict: 'EA',
-                    require:"ngModel",
                     templateUrl: 'plugins/base/templates/uploader.html',
                     replace: true,
                     scope: {
                         options: "="
                     },
+                    require: 'ngModel',
+                    link: function link(scope, element, attrs, ngModelController) {
+                    	setTimeout(function(){
+                    		scope.attachmentId = ngModelController.$viewValue;
+                    		scope.init();
+                    		scope.$watch("attachmentId",function(){
+                        		if(scope.attachmentId && !ngModelController.$viewValue){
+                        			ngModelController.$setViewValue(scope.attachmentId);
+                        		}
+                        	});
+                    	},0);
+                    	
+                    },
                     controller: ['$scope', '$attrs', '$element', 'utils', 'settings',
                         function ($scope, $attrs, $element, utils, settings) {
-                            var url = settings.uploadUrl;
-                            url = utils.getAbsUrl(url);
-                            var uploader = $scope.uploader = new FileUploader({
-                                url: url,
-                                autoUpload: true
-                            });
-                            $scope.apiPath = settings.apiPath;
-                            utils.async("GET", settings.getUuid).then(function (attId) {
-                                $scope.model = attId;
-                                uploader.formData = [{'attachmentId': $scope.model}];
-                                uploader.onSuccessItem = function (item, res, status, headers) {
-                                	item.uid = res.body.data[0].uid;
-                                    $scope.message = res.message;
-                                    //if (angular.isFunction(options.resolve)) {
-                                    //    options.resolve.apply();
-                                    //}
-                                };
-                            });
-
+	                    	var url = settings.uploadUrl;
+	                        url = utils.getAbsUrl(url);
+	                        var uploader = $scope.uploader = new FileUploader({
+	                            url: url,
+	                            autoUpload: true
+	                        });
+	                        
+	                        $scope.init = function(){
+	                            $scope.apiPath = settings.apiPath;
+	                            if(!$scope.attachmentId){
+		                            utils.async("GET", settings.getUuid).then(function (attId) {
+		                                $scope.attachmentId = attId;
+		                            });
+	                            }else{
+	                            	utils.async("GET", settings.getByAttIdUrl,{"attId":$scope.attachmentId}).then(function (rs) {
+	                            		$scope.items = rs.body;
+		                            });
+	                            }
+	                            uploader.formData = [{'attachmentId': $scope.attachmentId}];
+	                            uploader.onSuccessItem = function (item, res, status, headers) {
+	                            	item.uid = res.body.data[0].uid;
+	                                $scope.message = res.message;
+	                            };
+	                        }
+	                        
+	                        $scope.remove= function(item){
+	                        	utils.async("DELETE", settings.delByUid+"/"+item.uid).then(function (rs) {
+	                        		if(rs.body){
+	                        			if($scope.items){
+	    		                        	for(var i=0,size=$scope.items.length;i<size;i++){
+	    		                        		if($scope.items[i]==item){
+	    		                        			$scope.items.splice(i,1);
+	    		                        		}
+	    		                        	}
+	    	                        	}
+	                        		}else{
+	                        			console.log("后台删除出错!");
+	                        		}
+	                            });;
+	                        };
+	                        
                             //
                             //uploader.filters.push({
                             //    name: 'customFilter',
@@ -44,7 +78,6 @@
                             //if (angular.isFunction(options.resolve)) {
                             //    options.resolve.apply(uploader);
                             //}
-
                         }]
                 };
             }]);
