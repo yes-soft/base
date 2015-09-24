3f10b3da87146b6fcb66c5ddcd02e98b28b62fe2
qiongwei.cai@yes-soft.cn <245104777@qq.com>
2015-09-24 16:35:28 +0800

    date,dateTime,dateRange组合

---
+++ src/plugins/base/vendor/datepicker/gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp'),
+    fs = require("fs"),
+    del = require('del'),
+    concat = require('gulp-concat'), rename = require('gulp-rename'),
+    uglify = require('gulp-uglify');
+
+var inject = require('gulp-inject');
+var annotate = require('gulp-ng-annotate');
+var es = require('event-stream');
+var path = require('path');
+
+
+gulp.task('scripts', function () {
+    return gulp.src(
+        [
+            'bootstrap-datepicker.min.js',
+            'bootstrap-datetimepicker.min.js',
+            'daterangepicker.min.js'
+        ]
+    )
+        .pipe(concat('data.package.js'))
+        .pipe(uglify())
+        .pipe(rename({extname: '.min.js'}))
+        .pipe(gulp.dest(""));
+
+});
+
+gulp.task('css', function () {
+    return gulp.src([
+        'bootstrap-datetimepicker.min.css',
+        'datepicker.min.css',
+        'daterangepicker.min.css',
+    ])
+        .pipe(concat('date.package.min.css'))
+        .pipe(gulp.dest(""));
+});
