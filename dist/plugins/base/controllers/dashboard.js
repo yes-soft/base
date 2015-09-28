9c78422d1c43f124d12ad54bfe150256b37e48b0
qiongwei.cai@yes-soft.cn <245104777@qq.com>
2015-09-28 18:47:54 +0800

    update config

--- dist/plugins/base/controllers/dashboard.js
+++
@@ -1,7 +0,0 @@
-angular.module('app')
-    .controller('app.dashboard', ['$scope', '$stateParams', 'utils', 'settings',
-        function ($scope, $stateParams, utils, settings) {
-            var self = $scope;
-
-
-        }]);
