4fe2da883bb010d3b9c4d91dcd076f397f8846f6
qiongwei.cai@yes-soft.cn <245104777@qq.com>
2015-10-16 14:11:20 +0800

    dateRange修改

--- src/plugins/base/directives/datePicker.js
+++ src/plugins/base/directives/datePicker.js
@@ -40,6 +40,69 @@
                                     $(this).prev().focus();
                                 });
                             } else if (type == "daterange") {
+                                var locale = {};
+                                if (settings.language.toLowerCase().indexOf("cn") != -1) {
+                                    locale = {
+                                        "format": "YYYY-MM-DD",
+                                        "applyLabel": "应用",
+                                        "cancelLabel": "取消",
+                                        "daysOfWeek": [
+                                            "日",
+                                            "一",
+                                            "二",
+                                            "三",
+                                            "四",
+                                            "五",
+                                            "六"
+                                        ],
+                                        "monthNames": [
+                                            "一月",
+                                            "二月",
+                                            "三月",
+                                            "四月",
+                                            "五月",
+                                            "六月",
+                                            "七月",
+                                            "八月",
+                                            "九月",
+                                            "十月",
+                                            "十一月",
+                                            "十二月"
+                                        ],
+                                        "firstDay": 1
+                                    };
+                                }else{
+                                    locale =  {
+                                        "format": "MM/DD/YYYY",
+                                        "applyLabel": "Apply",
+                                        "cancelLabel": "Cancel",
+                                        "daysOfWeek": [
+                                            "Su",
+                                            "Mo",
+                                            "Tu",
+                                            "We",
+                                            "Th",
+                                            "Fr",
+                                            "Sa"
+                                        ],
+                                        "monthNames": [
+                                            "January",
+                                            "February",
+                                            "March",
+                                            "April",
+                                            "May",
+                                            "June",
+                                            "July",
+                                            "August",
+                                            "September",
+                                            "October",
+                                            "November",
+                                            "December"
+                                        ],
+                                        "firstDay": 1
+                                    };
+                                }
+
                                 var changeInScope = "model";
                                 if ($scope.changeInScope) {
                                     changeInScope = $scope.changeInScope;
@@ -47,27 +110,23 @@
                                 var from = $scope.$parent[changeInScope][$scope.nameFrom];
                                 var to = $scope.$parent[changeInScope][$scope.nameTo];
                                 var date = $(element).daterangepicker({
-                                    language: settings.language,
-                                    startDate: new moment(from),
-                                    endDate: new moment(to),
-                                    format: 'YYYY-MM-DD',
+                                    "locale": locale,
                                     'applyClass': 'btn-sm btn-success',
-                                    'cancelClass': 'btn-sm btn-default',
-                                    locale: {
-                                        applyLabel: 'Apply',
-                                        cancelLabel: 'Cancel'
-                                    }
-                                }, function (start, end) {
+                                    'cancelClass': 'btn-sm btn-default'
+                                }, function (start, end, label) {
                                     var str = start.format("YYYY-MM-DD"), end = end.format("YYYY-MM-DD");
                                     $scope.$parent[changeInScope][$scope.nameFrom] = str;
                                     $scope.$parent[changeInScope][$scope.nameTo] = end;
-                                    ngModelController.$setViewValue(str + " - " + end);
-                                    date.val(str + " - " + end);
+                                    ngModelController.$setViewValue(str + "~" + end);
+                                    date.val(str + "~" + end);
                                     //$scope.$parent[changeInScope][ngModelController.$viewName] = ;
                                 });
                                 if (from && to) {
-                                    date.val(from + " - " + to);
-                                    ngModelController.$setViewValue(from + " - " + to);
+                                    date.val(from + "~" + to);
+                                    ngModelController.$setViewValue(from + "~" + to);
+                                } else {
+                                    //date.val("");
+                                    //ngModelController.$setViewValue("");
                                 }
                                 $(element).prev().on("click", function () {
                                     $(this).next().focus();
